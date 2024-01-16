bb62ffc7a02ac070a7ffe91b7d7b7cf9920f48cb
Robert Koch <robert@kochie.io>
2024-01-16 12:49:40 +1100

    udapte to use react-tweet :

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,8 @@ import p5 from "../assets/images/5.jpeg";
 import josh1 from "../assets/images/josh1.jpeg";
 import josh2 from "../assets/images/josh2.jpeg";
 
+import { Tweet, TweetInReplyTo } from "react-tweet";
+
 export default function Home() {
   return (
     <div className="bg-slate-100 dark:bg-slate-800 dark:text-white text-center md:text-left">
@@ -46,25 +48,25 @@ export default function Home() {
         }}
       />
 
-      <div className="md:flex md:flex-col md:justify-center">
+      <div className="md:flex md:flex-col md:justify-center gap-6 md:mx-auto items-center">
         <div className="md:mt-40 text-5xl text-center p-5">
           I am <span className="italic font-bold mx-2">_not_</span> David Koch.
         </div>
-        <div className="md:mx-auto mx-3 my-20">
+        <div className="my-20">
           <Image src={David} alt="image of david koch from sunrise" />
         </div>
         <div className="flex justify-center">
           Look at this beautiful egg shaped man, now look at me.
         </div>
-        <div className="md:mx-auto mx-3 my-20 md:w-96">
+        <div className="md:w-96">
           <Image src={Me} alt="image of me robert koch" />
         </div>
-        <div className="flex justify-center md:w-[600px] md:mx-auto mx-3">
+        <div className="flex justify-center md:w-[600px] ">
           {
             "Don't be fooled by how much we're #twinning we couldn't be more different. Well we're both white Australian males, but David is 38 years older than me."
           }
         </div>
-        <div className="md:w-[600px] md:mx-auto mx-3 mt-10">
+        <div className="md:w-[600px]">
           {
             'As the chairman of the Port Adelaide Football Club David gets a lot of "fan" support. But sometimes in the heat of the moment his critics will mistake his twitter handle of '
           }
@@ -77,30 +79,29 @@ export default function Home() {
           </a>
           {"."}
         </div>
-        <div className="md:mx-auto mx-3 my-20">
-          <MichaelLynch />
+        <div className="">
+          {/* <MichaelLynch /> */}
+          <Tweet id="1114324112011214849" />
         </div>
-        <div className="md:mx-auto mx-3 mb-20">
-          <Spotty />
-        </div>
-        <div className="md:mx-auto mx-3">
-          <Leigh />
-        </div>
+
+        {/* <Spotty /> */}
+        <Tweet id="1242652582482292737" />
+        <Tweet id="1223362204310753280" />
+
+        <Tweet id="1437154932616949762" />
+
         <div className="flex justify-center md:w-[600px] mx-auto mt-10 mb-20">
           {"Oof Leigh, tell me how you really feel."}
         </div>
 
-        <div className="md:mx-auto mx-3">
-          <Michael />
-        </div>
+        <Tweet id="1277777946397143040" />
+
         <div className="flex justify-center md:w-[600px] md:mx-auto mx-3 mt-10 mb-20">
           {"Michael really doesn't like me :("}
         </div>
 
-        <div className="md:mx-auto mx-3">
-          <ArchieObserving />
-        </div>
-        <div className="flex justify-center md:w-[600px] md:mx-auto mx-3 mt-10 mb-20">
+        <Tweet id="1558592495532982272" />
+        <div className="flex justify-center md:w-[600px] md:mx-auto mx-3 mt-10 mb-20 text-center">
           {
             "Archie is upset a commercial TV show has no scientific data on it. May I suggest ABC News Archie?"
           }
@@ -111,21 +112,28 @@ export default function Home() {
             "And it's not just his detractors, politicians get it wrong...\n all. the. time."
           }
         </div>
-        <div className="md:mx-auto mx-3 mt-20">
-          <Jim />
-        </div>
-        <div className="md:mx-auto mx-3 my-20">
-          <MurrayWatt />
-        </div>
+
+        <Tweet id="1549503540821098496" />
+
+        <Tweet id="1549861881632157696" />
 
         <div className="flex justify-center md:w-[600px] mx-auto">
           {
             "But don't feel bad if you can't tell us apart. His co-host couldn't either."
           }
         </div>
-        <div className="md:mx-auto mx-3 my-20 md:w-[500px]">
-          <Sam />
+
+        <Tweet id="431138757220110337" />
+
+        <Tweet id="1729316763584856544" />
+
+        <div className="flex justify-center md:w-[600px] mx-auto">
+          {"I get in on the fun too."}
         </div>
+        <Tweet id="1729372641914699866" />
+
+        <span> But Alas, all good things must come to an end.</span>
+        <Tweet id="1663074550115074049" />
 
         <div className="flex justify-center">
           {
@@ -150,7 +158,7 @@ export default function Home() {
             "But hey it could be worse, at least no one thinks I'm this David Koch."
           }
         </div>
-        <div className="md:mx-auto mx-3 mt-20">
+        <div className="mb-40">
           <Image
             src={DavidKoch}
             alt="image of David Koch from Koch Brothers."
@@ -161,167 +169,4 @@ export default function Home() {
   );
 }
 
-const Jim = () => (
-  <>
-    <blockquote className="twitter-tweet">
-      <p lang="en" dir="ltr">
-        Looking forward to joining{" "}
-        <a href="https://twitter.com/kochie?ref_src=twsrc%5Etfw">@kochie</a> and
-        on{" "}
-        <a href="https://twitter.com/sunriseon7?ref_src=twsrc%5Etfw">
-          @sunriseon7
-        </a>{" "}
-        this morning{" "}
-        <a href="https://twitter.com/hashtag/auspol?src=hash&amp;ref_src=twsrc%5Etfw">
-          #auspol
-        </a>{" "}
-        <a href="https://twitter.com/hashtag/ausecon?src=hash&amp;ref_src=twsrc%5Etfw">
-          #ausecon
-        </a>
-      </p>
-      &mdash; Jim Chalmers MP (@JEChalmers){" "}
-      <a href="https://twitter.com/JEChalmers/status/1549503540821098496?ref_src=twsrc%5Etfw">
-        July 19, 2022
-      </a>
-    </blockquote>{" "}
-  </>
-);
 
-const Sam = () => (
-  <blockquote className="twitter-tweet">
-    <p lang="en" dir="ltr">
-      “<a href="https://twitter.com/T2hays?ref_src=twsrc%5Etfw">@T2hays</a>:{" "}
-      <a href="https://twitter.com/sam_armytage?ref_src=twsrc%5Etfw">
-        @sam_armytage
-      </a>{" "}
-      Tell <a href="https://twitter.com/kochie?ref_src=twsrc%5Etfw">@kochie</a>
-      <br /> &#39;Go the Crows&#39;”
-      <br />
-      <br />
-      Tell him yourself{" "}
-      <a href="https://twitter.com/kochie_online?ref_src=twsrc%5Etfw">
-        @kochie_online
-      </a>
-    </p>
-    &mdash; Samantha Armytage (@sam_armytage){" "}
-    <a href="https://twitter.com/sam_armytage/status/431138757220110337?ref_src=twsrc%5Etfw">
-      February 5, 2014
-    </a>
-  </blockquote>
-);
-
-const MichaelLynch = () => (
-  <>
-    <blockquote className="twitter-tweet">
-      <p lang="en" dir="ltr">
-        I assume{" "}
-        <a href="https://twitter.com/kochie_online?ref_src=twsrc%5Etfw">
-          @kochie_online
-        </a>{" "}
-        and <a href="https://twitter.com/PAFC?ref_src=twsrc%5Etfw">@PAFC</a> you
-        are merely trolling the rest of the world. If not, your delusional
-        levels of solipsism are breathtaking.{" "}
-        <a href="https://twitter.com/kochie?ref_src=twsrc%5Etfw">@kochie</a> you
-        have made a living as a financial journalist. If this is a measure of
-        your research, God help your readers and viewers!!
-      </p>
-      &mdash; Michael Lynch (@MickLynch_Age){" "}
-      <a href="https://twitter.com/MickLynch_Age/status/1114324112011214849?ref_src=twsrc%5Etfw">
-        April 6, 2019
-      </a>
-    </blockquote>
-  </>
-);
-
-const Spotty = () => (
-  <blockquote className="twitter-tweet">
-    <p lang="en" dir="ltr">
-      Seriously{" "}
-      <a href="https://twitter.com/kochie?ref_src=twsrc%5Etfw">@kochie</a>, as a{" "}
-      <a href="https://twitter.com/PAFC?ref_src=twsrc%5Etfw">@pafc</a> member
-      who is prepared to allow the club to keep my $500, these millionaire
-      footballers are crying poor and only prepared to take an effective pay cut
-      of 8%. Tell your players to step in line or shut up
-    </p>
-    &mdash; Spotty (@spottythompson){" "}
-    <a href="https://twitter.com/spottythompson/status/1242652582482292737?ref_src=twsrc%5Etfw">
-      March 25, 2020
-    </a>
-  </blockquote>
-);
-
-const Leigh = () => (
-  <blockquote className="twitter-tweet">
-    <p lang="en" dir="ltr">
-      Morning{" "}
-      <a href="https://twitter.com/kochie?ref_src=twsrc%5Etfw">@kochie</a> you
-      wanker <a href="https://t.co/qNxZaHLBQl">pic.twitter.com/qNxZaHLBQl</a>
-    </p>
-    &mdash; Leigh (@Leigh65399719){" "}
-    <a href="https://twitter.com/Leigh65399719/status/1437154932616949762?ref_src=twsrc%5Etfw">
-      September 12, 2021
-    </a>
-  </blockquote>
-);
-
-const Michael = () => (
-  <blockquote className="twitter-tweet">
-    <p lang="en" dir="ltr">
-      Kochie is everything that I hate about Australia. I a leaving once the
-      winter is over. Queensland winters are to die for. Have blanked out the 7
-      on my TV remote
-    </p>
-    &mdash; 💧Michael Q Todd ⛳ IOTA FAN l GOLFER l DREAMER (@michaelqtodd){" "}
-    <a href="https://twitter.com/michaelqtodd/status/1277777946397143040?ref_src=twsrc%5Etfw">
-      June 30, 2020
-    </a>
-  </blockquote>
-);
-
-const MurrayWatt = () => (
-  <blockquote className="twitter-tweet">
-    <p lang="en" dir="ltr">
-      Good to talk to{" "}
-      <a href="https://twitter.com/kochie?ref_src=twsrc%5Etfw">@Kochie</a> on{" "}
-      <a href="https://twitter.com/sunriseon7?ref_src=twsrc%5Etfw">
-        @Sunriseon7
-      </a>{" "}
-      about what we’re doing to ensure Australia stays free of foot and mouth
-      disease. Coming up on{" "}
-      <a href="https://twitter.com/ABCBreakfast?ref_src=twsrc%5Etfw">
-        @ABCBreakfast
-      </a>{" "}
-      after 7am.{" "}
-      <a href="https://t.co/At1keQYLgf">pic.twitter.com/At1keQYLgf</a>
-    </p>
-    &mdash; Senator Murray Watt (@MurrayWatt){" "}
-    <a href="https://twitter.com/MurrayWatt/status/1549861881632157696?ref_src=twsrc%5Etfw">
-      July 20, 2022
-    </a>
-  </blockquote>
-);
-
-const ArchieObserving = () => (
-  <blockquote className="twitter-tweet">
-    <p lang="en" dir="ltr">
-      Just watched{" "}
-      <a href="https://twitter.com/hashtag/Covid_19?src=hash&amp;ref_src=twsrc%5Etfw">
-        #Covid_19
-      </a>{" "}
-      update from{" "}
-      <a href="https://twitter.com/kochie?ref_src=twsrc%5Etfw">@kochie</a> on
-      Ch7.
-      <br />
-      Kochie&#39;s &quot;can we say it&#39;s over yet?&quot; was met with a
-      &quot;cautiously, yes&quot; response, followed by data illustrating the
-      very opposite, ie why we can expect new variants and multiple waves every
-      year.
-      <br />
-      There was NO health advice given.😡
-    </p>
-    &mdash; Archie O-Observing (@ArchieObserving){" "}
-    <a href="https://twitter.com/ArchieObserving/status/1558592495532982272?ref_src=twsrc%5Etfw">
-      August 13, 2022
-    </a>
-  </blockquote>
-);
